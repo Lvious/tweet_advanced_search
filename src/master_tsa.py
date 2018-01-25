fa8df738013e8491f9acd6a086fbefb8e04edd34
lvious <lvxq777@gmail.com>
2018-01-25 14:19:52 +0800

    init

--- src/master_tsa.py
+++ src/master_tsa.py
@@ -7,7 +7,7 @@ import time
 from config import get_spider_config
 _,db,r = get_spider_config()
 
-tweet_epoch = 1288806174657
+tweet_epoch = 1288834974657#1288806174657
 def get_task():
     from_date  =  "2018-01-22"
     to_date    =  "2018-01-23"
@@ -27,6 +27,6 @@ def get_task():
         }
         print(message)
         r.rpush("task:dataset",json.dumps(message))
-
+        
 if __name__ == '__main__':
 	get_task()
