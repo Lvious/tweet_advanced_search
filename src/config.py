206debe81bcda8ef0c669ec4028f54d6508f3674
lvious <lvxq777@gmail.com>
2018-01-30 22:44:32 +0800

    modify config.py

--- src/config.py
+++ src/config.py
@@ -1,16 +1,16 @@
 import pymongo
 import redis
-
+import os
 def get_spider_config():
     
     spider = "hello"
-    client = pymongo.MongoClient(host='54.210.116.149',port=27017)
+    client = pymongo.MongoClient(host=os.environ['MONGOHOST'],port=os.environ['MONGOPORT'])
     db = client.tweet_lv
-    r = redis.StrictRedis(host="54.161.160.206",port=6479,db=0)
+    r = redis.StrictRedis(host=os.environ['REDISHOST'],port=os.environ['REDISPORT'],db=0)
 
     return spider,db,r
 
 def get_collections_name():
-    SPIDER = "day_1"
+    SPIDER = "day"
     CLUSTER = "day_cluster"
     return SPIDER,CLUSTER
