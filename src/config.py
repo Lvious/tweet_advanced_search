c32d78bcb5fd8b73bd867c61f7af588ace718cb1
Lv <lvxq777@gmail.com>
2018-02-17 00:40:42 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ def get_spider_config():
     
     spider = "hello"
     client = pymongo.MongoClient(host=os.environ['MONGOHOST'],port=int(os.environ['MONGOPORT']))
-    db = client.tweet_lv
+    db = client.tweet_event
     r = redis.StrictRedis(host=os.environ['REDISHOST'],port=int(os.environ['REDISPORT']),db=0)
 
     return spider,db,r
